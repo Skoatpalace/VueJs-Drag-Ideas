3b2f759702689962ef9d141bb025da5651af19c3
Skoatpalace <richard.fages@gmail.com>
2020-04-10 10:10:40 +0200

    end courses

---
+++ src/components/IdeaCreate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent="create">
+      <label for="idea-title">Ideas Title</label>
+      <br />
+      <input id="idea-title" type="text" v-model="title" />
+      <br />
+      <button type="submit">Create</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    create() {
+      const idea = {
+        createAt: new Date(),
+        title: this.title
+      };
+      //this.$store.commit('CREATE_IDEAS', idea)
+      this.$store.dispatch("createIdea", idea);
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Welcome to Ideas.io</h1>
+    <IdeaCreate />
+    <Ideas />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import IdeaCreate from "@/components/IdeaCreate";
+import Ideas from "@/components/Ideas";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    IdeaCreate,
+    Ideas
   }
 };
 </script>
